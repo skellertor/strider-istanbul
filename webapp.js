b3d3e6efafdc5d40ce942e0cd34295b44c9c8a21
Sam Keller <skellertor@gmail.com>
2016-08-24 12:34:46 -0600

    reports endpoint

--- webapp.js
+++ webapp.js
@@ -1,10 +1,20 @@
 'use strict';
+var request = require('request');
 
 module.exports = {
   config: {},
   routes: function (app, context) {
     app.get('/report', function (req, res) {
-      res.json({project: req.project});
+      var org = req.params.org;
+      var repo = req.params.repo;
+      var branch = req.body.branch;
+      var protocol = req.protocol;
+      var hostName = req.get('Host');
+      request(protocol + '://' + hostName + '/' + org + '/' + repo + '/jobs', function (err, response, body) {
+        var returnObject = (typeof body);
+        var url = './.strider/data/'+ org + '-' + repo + '-' + branch;
+        res.json({params: returnObject});
+      });
     });
   },
   globalRoutes: function (app, context) {
