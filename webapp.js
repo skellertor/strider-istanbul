e29eda88e773779cafa3c06ecc8af551acd81c97
Sam Keller <skellertor@gmail.com>
2016-08-24 13:58:54 -0600

    reports endpoint

--- webapp.js
+++ webapp.js
@@ -12,7 +12,8 @@ module.exports = {
       var protocol = req.protocol;
       var hostName = req.get('Host');
       var project = org + '/' + repo;
-      var coveragLocation = '~/.strider/data/'+ org + '-' + repo + '-' + branch + '/job-' + '/coverage/index.html';
+      var jobId = context.strider_istanbul._id;
+      var coveragLocation = '~/.strider/data/'+ org + '-' + repo + '-' + branch + '/job-' + jobId + '/coverage/index.html';
       // res.render(coveragLocation);
       res.json({yes: context});
       // var jobsEndpoint = protocol + '://' + hostName + '/' + org + '/' + repo + '/jobs';
@@ -23,12 +24,6 @@ module.exports = {
   globalRoutes: function (app, context) {
     app.get('/report', function (req, res) {
       res.json({context: context});
-    });
-  },
-  listen: function(io, context) {
-    io.on('plugin.strider-istanbul.done', function (jobId) {
-      console.log('STRIDER DONE');
-      context.job_id = jobId;
     });
   }
 };
