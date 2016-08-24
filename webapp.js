8b13774fc54eefe91cbb2d7844451fc33e94fe21
Sam Keller <skellertor@gmail.com>
2016-08-24 13:07:38 -0600

    reports endpoint

--- webapp.js
+++ webapp.js
@@ -11,10 +11,11 @@ module.exports = {
       var branch = req.body.branch;
       var protocol = req.protocol;
       var hostName = req.get('Host');
-      request(protocol + '://' + hostName + '/' + org + '/' + repo + '/jobs', function (err, response, body) {
+      var jobsEndpoint = protocol + '://' + hostName + '/' + org + '/' + repo + '/jobs';
+      console.log('JOBS',jobsEndpoint)
+      request(jobsEndpoint, function (err, response, body) {
         console.log('BODY', body);
         var latestJobId = JSON.parse(body)[0];
-        console.log('LATEST',latestJobId);
         var coveragLocation = '~/.strider/data/'+ org + '-' + repo + '-' + branch + '/job-' + latestJobId + '/coverage/index.html';
         res.render(coveragLocation);
       });
